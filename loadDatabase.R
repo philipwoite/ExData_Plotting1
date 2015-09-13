a63888523e89e11031a6ae0e62ee47229a216799
Philip Woite <philip.woite@gmail.com>
2015-09-13 20:28:50 +0200

    Update with references

--- loadDatabase.R
+++ loadDatabase.R
@@ -1,6 +1,9 @@
 library(data.table)
 dataset <- read.table("household_power_consumption.txt",sep = ";", na.strings = "?")
 dataset[,1] <- as.Date(dataset[,1], "%d/%m/%Y")
+
+# colnames reference: Comment by Steven Beaupré : http://stackoverflow.com/questions/28240183/r-data-frame-headers-based-on-existing-row-containing-text-and-numbers
+
 colnames(dataset) <- lapply(dataset[1,], as.character)
 colnames(dataset)[1] <- "Date"
 
--- plot1.R
+++ plot1.R
@@ -1,7 +1,10 @@
 library(dplyr)
 
+# How to save reference: http://stackoverflow.com/questions/7144118/how-to-save-a-plot-as-image-on-the-disk
+
 png(file="plot1.png",width=480,height=480)
 
+# Axes and titles reference: http://www.statmethods.net/advgraphs/axes.html
 
 hist(as.numeric(workingDataset[,"Global_active_power"]),  xlab = "Global Active Power (kilowatts)", ylab = "Frequency", main = "Global Active Power", col = "red")
 dev.off()
