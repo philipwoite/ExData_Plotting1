d15797ffedc942cf9d9e412bc9c2fc4b80cce2e4
Philip Woite <philip.woite@gmail.com>
2015-09-13 21:20:06 +0200

    Plot 2 second attempt

--- plot2.R
+++ plot2.R
@@ -6,6 +6,14 @@ png(file="plot2.png",width=480,height=480)
 
 # Axes and titles reference: http://www.statmethods.net/advgraphs/axes.html
 
-plot(workingDataset$Date,workingDataset$Global_active_power, xlab = "", ylab = "Global Active Power (kilowatts)")
+#plot(workingDataset$Date,workingDataset$Global_active_power, type= "l", xlab = "", ylab = "Global Active Power (kilowatts)")
+#axis(side = 1, at=c(0:2))
+#dev.off()
 
-dev.off()
+# http://redheadedstepdata.io/a-few-simple-plots-in-r/
+workingDatasetSplit <- split(workingDataset, workingDataset$Time, drop = TRUE)
+intervalGlobalActivePower <- sapply(workingDatasetSplit, function(workingDataset) mean(workingDataset$Global_active_power))
+
+intervalGlobalActivePower <- sapply(workingDatasetSplit, function(x) mean(as.numeric(x$Global_active_power)))
+plot(intervalGlobalActivePower, type = "l",     main = "",      xlab = "",     ylab = "Global Active Power (kilowatts)")
+     dev.off()
